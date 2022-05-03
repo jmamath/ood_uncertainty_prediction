a4b71493210f493bbfb93c8ea744fcdef9d66f63
Jean-Michel <you@example.com>
2022-05-03 18:08:46 +0000

    Visualization output results

--- visualization.py
+++ visualization.py
@@ -10,21 +10,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import ImageGrid
+import os
 
 def normalize(data):
     return (data - data.min())/ (data.max() - data.min())
 
-def plot_distance_to_acc(distance, accuracies):
+def plot_distance_to_acc(distance, accuracies, plot_name):
     gap = np.abs(accuracies-accuracies[0])
     sns.regplot(x=normalize(distance), y=gap)
     plt.ylim([0,1])
     plt.ylabel("| acc(T)-acc(S) |")
     plt.xlabel("Dataset distance")
     plt.title("Loss of performance as dataset shifts")
-    # plt.savefig("MNIST-C-1norm", dpi=200)
+    plt.savefig("{}".format(plot_name), dpi=200)
     plt.show()
 
-def plot_atc_differences(estimation, accuracies):
+def plot_atc_differences(estimation, accuracies, plot_name):
     """Because estimations were designed to approximate accuracies in percent between 0 and 100
     we need to"""
     gap = np.abs(accuracies-accuracies[0])
@@ -34,7 +35,7 @@ def plot_atc_differences(estimation, accuracies):
     plt.ylabel("| acc(T)-acc(S) |")
     plt.xlabel("ATC")
     plt.title("Loss of performance as dataset shifts")
-    # plt.savefig("MNIST_C_ATC", dpi=200)
+    plt.savefig("{}".format(plot_name), dpi=200)
     plt.show()
     
     
@@ -43,9 +44,9 @@ h_distances = np.load("mnist_h_distances.npy")
 labelwise_h_distances = np.load("mnist_labelwise_h_distances.npy")
 atc = np.load("mnist_atc.npy")
 
-plot_distance_to_acc(h_distances, accuracies)
-plot_distance_to_acc(-labelwise_h_distances, accuracies)
-plot_atc_differences(atc, accuracies)
+plot_distance_to_acc(h_distances, accuracies, "mnist_h_distance_plot")
+plot_distance_to_acc(-labelwise_h_distances, accuracies, "mnist_labelwise_h_distance_plot")
+plot_atc_differences(atc, accuracies, "mnist_atc_plot")
     
 ######################## VISUALIZE DIVERGENCE MATRICES ########################
 
